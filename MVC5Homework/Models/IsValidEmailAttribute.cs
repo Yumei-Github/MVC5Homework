1449bf0559dea354b83139affd8924f2233fdcea
branda <s2004120@yahoo.com.tw>
2020-10-29 21:36:04 +0800

    對於 Create 與 Edit 表單要套用欄位驗證 (必填、Email格式、欄位長度限制等驗證)

---
+++ MVC5Homework/Models/IsValidEmailAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace MVC5Homework.Models
+{
+    public class IsValidEmailAttribute : DataTypeAttribute
+    {
+        public IsValidEmailAttribute() : base(DataType.Text)
+        {
+            ErrorMessage = "Email格式不對!";
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+
+            string email = value.ToString();
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            if (match.Success)
+                return true;
+            else
+                return false;
+        }
+    }
+}
